a88aecf310ab25386bf94a2174607ef2f7bf97d6
aniruddhaacharya <aniruddhaacharya59@gmail.com>
2025-10-27 17:04:46 +0530

    Testing file changes

--- src/components/__tests__/PokemonTile.test.tsx
+++ src/components/__tests__/PokemonTile.test.tsx
@@ -1,7 +1,13 @@
 import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
 import PokemonTile from "../PokemonTile";
 
 test("renders Pokémon name", () => {
-  render(<PokemonTile name="pikachu" image="/pikachu.png" />);
+  render(
+    <MemoryRouter>
+      <PokemonTile name="pikachu" image="/pikachu.png" />
+    </MemoryRouter>
+  );
+
   expect(screen.getByText(/Pikachu/i)).toBeInTheDocument();
 });
